bddd8055bc6f283cb2806c089a1fd1e0250866b3
maomao5959 <595971831@qq.com>
2021-07-23 13:36:26 +0800

    debug

--- common/http.interceptor.js
+++ common/http.interceptor.js
@@ -1,16 +1,16 @@
-//正式APPID： 测试APPID： wx8dad6b1612dbffd3
+//正式APPID： 测试APPID： 
 //公共js
 import md5 from "uview-ui/libs/function/md5";
 //变量
-const baseUrl = "https://wxapp.test.cdhzbc.net"; //测试版接口地址
-// const baseUrl = "http://192.168.1.164"; //开发本地接口地址
+const baseUrl = ""; //测试版接口地址
+// const baseUrl = ""; //开发本地接口地址
 
-const app_key = '672fa5fa-5fde-4e28-bca7-2caeca62df21'; //api密钥
+const app_key = ''; //api密钥
 const MSHttp = {
-	getTimestamp: function() {
+	getTimestamp: function () {
 		return Math.round(new Date().getTime() / 1000);
 	},
-	getNonce: function(length) {
+	getNonce: function (length) {
 		if (length == null) length = 32;
 		var str = "",
 			pos = 0,
@@ -26,17 +26,17 @@ const MSHttp = {
 		}
 		return str;
 	},
-	getOrderWithData: function(dic) {
+	getOrderWithData: function (dic) {
 		if (!dic) return '';
 		var result = "";
-		var sdic = Object.keys(dic).sort(function(a, b) {
+		var sdic = Object.keys(dic).sort(function (a, b) {
 			return a.localeCompare(b)
 		});
 		var value = "";
 		for (var ki in sdic) {
 			if (dic[sdic[ki]] == null) {
 				value = ""
-			} else if (typeof(dic[sdic[ki]]) == "undefined") {
+			} else if (typeof (dic[sdic[ki]]) == "undefined") {
 				value = ""
 			} else {
 				value = dic[sdic[ki]];
@@ -46,12 +46,12 @@ const MSHttp = {
 		}
 		return result.replace(/\s/g, "");
 	},
-	getSign: function(queryData, timestamp, nonce) {
+	getSign: function (queryData, timestamp, nonce) {
 		var data = MSHttp.getOrderWithData(queryData);
 		return md5.md5(timestamp + nonce + app_key + data);
 	},
 	apiUrl(url) {
-		return url+'/api';
+		return url + '/api';
 	},
 }
 
@@ -80,7 +80,7 @@ const install = (Vue, vm) => {
 			'signature': signature,
 			'token': token || "",
 		};
-		config.header =header;
+		config.header = header;
 		return config;
 		// 如果return一个false值，则会取消本次请求
 		// if(config.url == '/user/rest') return false; // 取消某次请求
